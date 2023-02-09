8fcdd64a27d436613a613831b52a2b595b856e01
Ovhas <dovakinbaradat@gmail.com>
2023-02-09 16:40:53 +0100

    :dizzy: Add logic of close modal btn

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const content = document.querySelector(".content");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,17 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close modal form
+document.querySelector(".close").addEventListener("click", () => {
+  content.classList.add("content--is-closed");
+});
+
+// when animation in "content" DOM element is finished
+content.addEventListener('animationend', (event) => {
+  // if this is the end of modalclose animation
+  if (event.animationName === "modalclose") {
+    modalbg.style.display = "none";
+    content.classList.remove("content--is-closed");
+  }
+});
 
